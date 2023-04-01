3898c13e718e2f5b277722a2e62e84c00561652b
202001200 <202001200@daiict.ac.in>
2023-04-01 15:42:05 +0530

    footer updated

--- Renting_FrontEnd/src/components/footer/footer.js
+++ Renting_FrontEnd/src/components/footer/footer.js
@@ -3,7 +3,7 @@ import './style.css';
 const Footer = () => {
     return (
         <div className='Footer'>
-            <h1>Renting System &copy; 2021</h1>
+            <h1>RentBuddy &copy; 2023</h1>
         </div>
     );
 };
