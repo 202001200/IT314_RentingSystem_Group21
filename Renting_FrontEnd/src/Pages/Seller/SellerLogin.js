26b29a3e8f7b9984b0bf00e569bcbbe900d6885c
Vikaspatni <202001192@daiict.ac.in>
2023-04-02 20:14:00 +0530

    Seller Login page

--- Renting_FrontEnd/src/Pages/Seller/SellerLogin.js
+++ Renting_FrontEnd/src/Pages/Seller/SellerLogin.js
@@ -1,7 +1,107 @@
-import React from 'react';
+// import React from 'react';
 
-const SellerLogin = () => {
-    return <div></div>;
+// const SellerLogin = () => {
+//     return <div></div>;
+// };
+
+// export default SellerLogin;
+import React, { useState } from 'react';
+import './style.css';
+import { Link, useHistory } from 'react-router-dom';
+import emailOutline from '@iconify-icons/mdi/email-outline';
+import lockOutline from '@iconify-icons/mdi/lock-outline';
+import signinposter2 from '../../Assets/signinposter2.png';
+import LoginInput from '../../Components/Input/LoginInput';
+import axios from 'axios';
+import { useAlert } from 'react-alert';
+
+const SellerLogin = (props) => {
+    const alert = useAlert();
+    let history = useHistory();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleInputEmail = (event) => {
+        setEmail(event.target.value);
+    };
+    const handleInputPassword = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleOnClick = () => {
+        axios
+            .post('https://rentingsystem.herokuapp.com/seller/login', {
+                email: email,
+                password: password,
+            })
+            .then(function (response) {
+                const data = response.data;
+                if (data.error) {
+                    alert.error(data.msg);
+                    localStorage.removeItem('seller');
+                    localStorage.removeItem('auth_token');
+                } else {
+                    alert.success('Welcome Seller');
+                    localStorage.setItem('seller', true);
+                    localStorage.setItem('auth_token', data.auth_token);
+                    props.handleClick();
+                    return history.push('./../');
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
+
+    return (
+        <div className='SellerLogin-body'>
+            <div className='SellerLogin-content'>
+                <div className='SellerLogin-text-body'>
+                    <div className='SellerLogin-text'>Seller sign in</div>
+                </div>
+                <div className='SellerLogin-input-main'>
+                    <LoginInput
+                        icon={emailOutline}
+                        placeholder={'E-mail'}
+                        type={'text'}
+                        handleInput={handleInputEmail}
+                    />
+                    <LoginInput
+                        icon={lockOutline}
+                        placeholder={'Password'}
+                        type={'password'}
+                        handleInput={handleInputPassword}
+                    />
+                    <div className='SellerLogin-remember'>
+                        <input
+                            type='checkbox'
+                            className='SellerLogin-checkbox'
+                        />
+                        <span className='SellerLogin-remember-text'>
+                            Remember me
+                        </span>
+                    </div>
+                    <div className='SellerLogin-login' onClick={handleOnClick}>
+                        Log in
+                    </div>
+                </div>
+                <div className='SellerLogin-create'>
+                    <Link to='./register'>Create an account</Link>
+                </div>
+            </div>
+            <div className='SellerLogin-logo'>
+                <img
+                    src={signinposter2}
+                    alt={'SignInPoster'}
+                    className='SellerLogin-poster'
+                />
+                <div className='SellerLogin-create2'>
+                    <Link to='../buyer/login'>Sign in as a Buyer</Link>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default SellerLogin;
