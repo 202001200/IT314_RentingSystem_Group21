3b21e0ede9a26d3dde3c5b93bb6ce201a62be288
ParekhKunj <124246644+ParekhKunj@users.noreply.github.com>
2023-04-05 19:13:35 +0530

    Update app.js

--- Renting_backend/config/app.js
+++ Renting_backend/config/app.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://KunjParekh:xyz123@cluster0.stcddrp.mongodb.net/?retryWrites=true&w=majority/RentingSystem")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("Connected to server..."))
 .catch((err) => console.log(err));
