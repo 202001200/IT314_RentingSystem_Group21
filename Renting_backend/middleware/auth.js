f93f06dd4de0e5d5e4826f01017647ffe63fb201
Kadam-Darji <202001198@daiict.ac.in>
2023-04-05 22:35:56 +0530

    Added token verification code

---
+++ Renting_backend/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = (req,res,next) =>{
+    const token = req.header('auth-token');
+    if(!token) return res.status(401).sned({
+        msg:"Access denied"
+    })
+}
+
+try{
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.buyer = verified;
+    next();
+}
+catch(err){
+    return res.status(400).json({
+        msg:"Please Login again"
+    })
+}
