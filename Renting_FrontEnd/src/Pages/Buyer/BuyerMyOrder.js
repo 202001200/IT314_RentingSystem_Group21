d9c196ed006a8373ee7fdd0d74a466d02a288420
ShivBaria <202001222@daiict.ac.in>
2023-04-05 17:37:13 +0530

    BuyerMyOrder_Page

--- Renting_FrontEnd/src/Pages/Buyer/BuyerMyOrder.js
+++ Renting_FrontEnd/src/Pages/Buyer/BuyerMyOrder.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from 'react'
+import GeneralCard from '../../components/Cardview/GeneralCard';
+
 
 const BuyerMyOrder = () => {
-    return <div>Buyer My Order</div>;
-};
+    const cards = [];
+        for (var i = 0; i < 5; i++) {
+            cards.push(<GeneralCard key={i} />);
+        }
+        return (
+            <div className='main-body' >
+                <div className='Main-card'>{cards}</div>
+            </div>
+        )
+
+}
 
 export default BuyerMyOrder;
