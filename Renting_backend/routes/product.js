58dbc1f9c5bccdd02a7f100df258a3bde7ba188e
Kadam-Darji <202001198@daiict.ac.in>
2023-04-24 17:20:01 +0530

    Added API to get Products from a category and id

--- Renting_backend/routes/product.js
+++ Renting_backend/routes/product.js
@@ -3,15 +3,36 @@ const router = express.Router();
 const Product = require('../models/Product.js');
 const { check, validationResult } = require('express-validator');
 
+router.get('/:category',async(req,res)=>{
+    try{
+        const products = await Product.find({
+            category:{
+                $eq:req.params.category
+            }
+        })
+        res.send(products);
+    }
+    catch(err){
+        res.statusCode = 500;
+        res.send({
+            error:true,
+            msg: err.message
+        })
+    }
+})
+
 router.get('/', async (req, res) => {
     try {
-        const products = await Product.find({});
+        const products = await Product.find({}).sort({
+            createdAt:'desc'
+        });
         res.send({
             error: false,
             product: products,
         });
     } catch (err) {
         console.error(err);
+        res.statusCode = 500;
         res.send({
             error: true,
             msg: err.message,
@@ -19,4 +40,17 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id',async(req,res)=>{
+    try{
+        const products = Product.findById(req.params.id);
+        res.send(products);
+    }
+    catch(err){
+        res.statusCode = 500;
+        res.send({
+            error:true,
+            msg:err.message
+        })
+    }
+})
 module.exports = router;
