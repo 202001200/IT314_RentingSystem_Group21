429aea8f916e0db81f4a1ef3a06ed6e91addf6e2
Kadam-Darji <81846811+Kadam-Darji@users.noreply.github.com>
2023-04-24 22:45:31 +0530

    Delete Renting_backend/config directory

--- Renting_backend/config/app.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb+srv://KunjParekh:xyz123@cluster0.stcddrp.mongodb.net/?retryWrites=true&w=majority/RentingSystem")
-.then(() => console.log("Connected to server..."))
-.catch((err) => console.log(err));
