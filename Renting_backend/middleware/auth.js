1faedf4382b13b5294b250d1673f4ed25fa7ec15
Kadam-Darji <202001198@daiict.ac.in>
2023-04-24 15:25:04 +0530

    fixed naming conventions

--- Renting_backend/middleware/auth.js
+++ Renting_backend/middleware/auth.js
@@ -2,18 +2,18 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req,res,next) =>{
     const token = req.header('auth-token');
-    if(!token) return res.status(401).sned({
+    if(!token) return res.status(401).send({
         msg:"Access denied"
     })
-}
 
 try{
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.buyer = verified;
+    req.lender = verified;
     next();
 }
 catch(err){
     return res.status(400).json({
         msg:"Please Login again"
     })
+}
 }
