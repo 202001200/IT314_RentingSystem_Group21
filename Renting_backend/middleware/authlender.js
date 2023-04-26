1371cc7e0e70e20123370407fe94581f7cdff7e8
Kadam-Darji <81846811+Kadam-Darji@users.noreply.github.com>
2023-04-26 21:03:05 +0530

    Update authlender.js

--- Renting_backend/middleware/authlender.js
+++ Renting_backend/middleware/authlender.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-
+require('dotenv').config();
 module.exports = (req,res,next) =>{
     const token = req.header('auth-token');
     if(!token) return res.status(401).send({
