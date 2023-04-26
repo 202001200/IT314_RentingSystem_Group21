690caa45d2443ff78b882abd9556e32302f9dc1d
Ayush Gandhi <202001200@daiict.ac.in>
2023-04-26 14:07:35 +0530

    APIs for wishlist and request

--- Renting_FrontEnd/src/components/Button/Button.js
+++ Renting_FrontEnd/src/components/Button/Button.js
@@ -1,42 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './style.css';
 import { Icon } from '@iconify/react';
 import { useHistory } from 'react-router-dom';
+
 const Button = (props) => {
   let history = useHistory();
   const click = () => {
-      if (props.handleClick) {
-          if (props.name === 'Cancel') {
-              return history.push('./../');
-          }
-          props.handleClick();
-          if (
-            props.name === 'Wishlist' ||
-            props.name === 'Request' ||
-            props.name === 'Accept' ||
-            props.name === 'Decline'
-          ) {
-              return;
-              } else if (props.name === 'Place Order') {
-                history.push('./order');
-          }
-          return history.push('./../');
+    if (props.handleClick) {
+      if (props.name == 'Cancel') {
+        return history.push('./../');
       }
+      props.handleClick();
+      if (props.name == 'Wishlist') {
+        return history.push('./wishlist');
+      }
+      return history.push('./../');
+    }
   };
 
-    return (
-      <div className='Button-main' onClick={click}>
-          <div className='Button-body'>
-              <div className='Button-icon-body'>
-                  <Icon icon={props.icon} className='Button-main-icon' />
-              </div>
-              <p className='Button-name'> {props.name}</p>
-          </div>
+  return (
+    <div className='Button-main' onClick={click}>
+      <div className='Button-body'>
+        <div className='Button-icon-body'>
+          <Icon icon={props.icon} className='Button-main-icon' />
+        </div>
+        <p className='Button-name'> {props.name}</p>
       </div>
-  
+    </div>
   );
 };
-
 
 Button.defaultProps = {
   name: 'Add to cart',
